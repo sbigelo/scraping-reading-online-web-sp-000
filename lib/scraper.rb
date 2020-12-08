26765b22a9843ce027e6cd31ad1097182fafe7cc
Sean Bigelow <sean.bigelow1@gmail.com>
2020-12-08 20:19:55 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,7 +1,9 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 
 html = open(https://edmidentity.com/live/livestream-tracker/)
 doc = Nokogiri::HTML(open(https://edmidentity.com/live/livestream-tracker/))
 p doc.css("th.1350945980R2").text
+binding.pry
